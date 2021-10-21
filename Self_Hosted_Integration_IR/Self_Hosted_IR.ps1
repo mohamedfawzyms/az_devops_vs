c6d1eb9ea8c802cbe435422d8476ca2ce8ebde9e
Mohamed Fawzy <mohamed.fawzy@microsoft.com>
2021-10-22 00:49:33 +0200

    commit on branch1

--- Self_Hosted_Integration_IR/Self_Hosted_IR.ps1
+++ Self_Hosted_Integration_IR/Self_Hosted_IR.ps1
@@ -3,8 +3,7 @@ function Create-Self-Hosted {
         [string]$Self_Hosted_IR_Name = "Test-self-hosted-ir-02",
         [string]$Resource_Group_Name = "arm_rg",
         [string]$Workspace_Name = "syn-m-str-pe",
-        [string]$Self_Hosted_IR_Type = "SelfHosted",
-        [string]$SubscriptionName = "MF_Azure_Subscription_1000"
+        [string]$Self_Hosted_IR_Type = "SelfHosted"
     )
     Set-AzSynapseIntegrationRuntime -Name $Self_Hosted_IR_Name -ResourceGroupName $Resource_Group_Name -workspacename $Workspace_Name -Type $Self_Hosted_IR_Type
     $key = Get-AzSynapseIntegrationRuntimeKey -Name $Self_Hosted_IR_Name -ResourceGroupName $Resource_Group_Name -workspacename $Workspace_Name
